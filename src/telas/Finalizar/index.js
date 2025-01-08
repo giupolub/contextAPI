77a00ff7bf937ca9feabd16d1416adc704aa49db
Giuseppe Poluboiarinov <giuseppe.po@hotmail.com>
2025-01-08 19:44:27 -0300

    Criadas as telas de Resumo e Finalizar.

---
+++ src/telas/Finalizar/index.js
@@ -0,0 +1,23 @@
+import { Text, View, StatusBar, TouchableOpacity } from 'react-native';
+import { estilos } from './estilos';
+import { useContext } from 'react';
+import { TemaContext } from '../../contexts/TemaContext';
+
+export default function Finalizar({ navigation }) {
+
+  const { temaEscolhido } = useContext(TemaContext)
+
+  const estilo = estilos(temaEscolhido)
+
+  return (
+    <View style={estilo.container}>
+      <StatusBar />
+
+      <TouchableOpacity style={estilo.botao}
+        onPress={() => navigation.navigate("Principal")}>
+        <Text style={estilo.textoBotao}>Finalizar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
